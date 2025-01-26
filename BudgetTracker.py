993143eb128a3879daaf42ab2c983611c9d4bae7
Rishi Ram Rijal <ramrijalrishi@gmail.com>
2025-01-26 13:27:45 +0200

    added two functionalities add budget account and remove bugdget account and some comments

--- BudgetTracker.py
+++ BudgetTracker.py
@@ -1,6 +1,7 @@
 from email_validator import validate_email, EmailNotValidError
-
 class Person:
+
+    # validate the email using python email_validator
     @staticmethod
     def is_valid_email(email):
         try:
@@ -11,18 +12,23 @@ class Person:
             print(f"Invalid email: {e}")
             return False
 
+    #creating a new object of person class with name and email
     def __init__(self, name, email):
         if not self.is_valid_email(email):
             raise ValueError("Cannot create Person object: Invalid email address.")
         self._name = name
         self._email = email
+        # initializing budget accounts to empty list to add accounts
+        self._budget_accounts = []
 
+    # method to change the person name
     def change_name(self, new_name):
         if new_name != self._name:
             self._name = new_name
         else:
             print("You already have that name")
 
+    #method to change the email afterwards
     def change_email(self, new_email):
         if not self.is_valid_email(new_email):
             print("Invalid email, cannot update.")
@@ -32,16 +38,23 @@ class Person:
             print("Email updated successfully.")
         else:
             print("This email is already set.")
+    
+    # add budget account
+    def add_budget_account(self, budget_account):
+        if budget_account not in self._budget_accounts:
+            self._budget_accounts.append(budget_account)
+            print("Budget Account added successfully")
+        
+        else:
+            print("Budget Account already exists")
+    
+    # remove budget account
+    def remove_budget_account(self, budget_account):
+        if budget_account not in self._budget_accounts:
+            print(f"There is not budget catagory with the name {budget_account}")
+        else:
+            self._budget_accounts.remove(budget_account)
+            print("Removed account Successfully")
 
-# Example Usage
-try:
-    person = Person("John Doe", "example@rishi.com")
-    print(f"Person created: {person._name}, {person._email}")
-except ValueError as e:
-    print(e)
 
-# Attempting to create a person with an invalid email
-try:
-    invalid_person = Person("Jane Doe", "invalid-email")
-except ValueError as e:
-    print(e)
+
